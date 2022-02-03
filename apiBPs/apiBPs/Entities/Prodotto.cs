c6092d0e9e91f03bae447b0fc197b62cde7d968a
zaccariaMajid <62284472+zaccariaMajid@users.noreply.github.com>
2022-02-03 12:30:56 +0100

    aggiunta api

---
+++ apiBPs/apiBPs/Entities/Prodotto.cs
@@ -0,0 +1,10 @@
+﻿namespace apiBPs.Entities
+{
+    public class Prodotto
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string prezzo { get; set; }
+        public string piattaforma { get; set; }
+    }
+}
---
+++ apiBPs/apiBPs/Startup.cs
@@ -0,0 +1,62 @@
+using apiBPs.Context;
+using apiBPs.Prodotti;
+using apiBPs.Repository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiBPs
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<DapperContext>();
+            services.AddScoped<IProductsRepository, ProductsRepository>();
+            services.AddControllers();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            app.UseAuthentication();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(x=>x
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
