f60153b755b0dba265f2e3f6a3f2260267509837
zaccariaMajid <zaccariam45@gmail.com>
2022-02-09 19:12:53 +0100

    Aggiornamento API

---
+++ API_pcto/API_pcto/Controllers/ProductsController.cs
@@ -0,0 +1,48 @@
+﻿using API_pcto.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace API_pcto.Controllers
+{
+    [Route("api/prodotti")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        
+        private readonly IProductsRepository _productsRepo;
+        public ProductsController(IProductsRepository productsRepo)
+        {
+            _productsRepo = productsRepo;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            try
+            {
+                var products = await _productsRepo.GetProducts();
+                return Ok(products);
+            }
+            catch (Exception ex)
+            {
+                //log error
+                return StatusCode(500, ex.Message);
+            }
+        }
+        [HttpPost("{nomeProdottoDaCercare}", Name = "nomeProdottoDaCercare")]
+        public async Task<IActionResult> CreateProduct(string nomeProdottoDaCercare)
+        {
+            try
+            {
+                await _productsRepo.CreateProduct(nomeProdottoDaCercare);
+                return StatusCode(200);
+            }
+            catch (Exception ex)
+            {
+                //log error
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+    }
+}
---
+++ API_pcto/API_pcto/Startup.cs
@@ -0,0 +1,69 @@
+using API_pcto.Context;
+using API_pcto.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_pcto
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddSingleton<DapperContext>();
+            services.AddScoped<IProductsRepository, ProductsRepository>();
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API_pcto", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API_pcto v1"));
+            }
+
+
+            app.UseCors(x => x
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
