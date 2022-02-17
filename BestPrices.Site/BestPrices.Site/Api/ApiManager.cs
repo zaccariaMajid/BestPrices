982f92dd810e1a4defcbcab8b9def4eebfdc6dec
nicosonzo03 <62284472+zaccariaMajid@users.noreply.github.com>
2022-02-17 14:54:46 +0100

    Aggiornamento eseguito da @nicosonzo03
    Co-Authored-By: nicosonzo03 <62284620+nicosonzo03@users.noreply.github.com>

--- BestPrices.Site/BestPrices.Site/Api/ApiManager.cs
+++ BestPrices.Site/BestPrices.Site/Api/ApiManager.cs
@@ -16,34 +16,36 @@ namespace BestPrices.Site.Api
         {
             //api url = somee
 
-            //string apiUrl = ""; // + text in url
-            //string jsonString = string.Empty;
-            //HttpWebRequest request = (HttpWebRequest)WebRequest.Create(apiUrl);
-            //using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-            //using (Stream stream = response.GetResponseStream())
-            //using (StreamReader reader = new StreamReader(stream))
-            //    jsonString = reader.ReadToEnd();
-            //return ToApiResult(JsonConvert.DeserializeObject<Result>(jsonString)).Data.ToList();
+            string apiUrl = "https://localhost:44349/Products/" + text; // + text in url aggiungere in appconfig
+            string jsonString = string.Empty;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(apiUrl);
+            string token = ""; // aggiungere in appconfig
+            request.Headers.Add("Authorization", $"Bearer {token}");
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+                jsonString = reader.ReadToEnd();
+            return ToApiResult(JsonConvert.DeserializeObject<Result>(jsonString)).Data.ToList();
 
-            return new List<Product>()
-            {
-                new Product()
-                {
-                    Name = "Pallone",
-                    Price = "€20.00",
-                    PathPhoto = "/img/prova.jpg",
-                    Link = "https://docs.microsoft.com/it-it/aspnet/core/security/gdpr?view=aspnetcore-5.0",
-                    IdEcommerce = "A"
-                },
-                new Product()
-                {
-                    Name = "Casa",
-                    Price = "€200.00",
-                    PathPhoto = "http://www.starcoppe.it/images/grafica-immagine-b.jpg",
-                    Link = "https://docs.microsoft.com/it-it/aspnet/core/security/gdpr?view=aspnetcore-5.0",
-                    IdEcommerce = "B"
-                }
-            };
+            //return new List<Product>()
+            //{
+            //    new Product()
+            //    {
+            //        Name = "Pallone",
+            //        Price = "€20.00",
+            //        PathPhoto = "/img/prova.jpg",
+            //        Link = "https://docs.microsoft.com/it-it/aspnet/core/security/gdpr?view=aspnetcore-5.0",
+            //        IdEcommerce = "A"
+            //    },
+            //    new Product()
+            //    {
+            //        Name = "Casa",
+            //        Price = "€200.00",
+            //        PathPhoto = "http://www.starcoppe.it/images/grafica-immagine-b.jpg",
+            //        Link = "https://docs.microsoft.com/it-it/aspnet/core/security/gdpr?view=aspnetcore-5.0",
+            //        IdEcommerce = "B"
+            //    }
+            //};
         }
 
         ApiResult ToApiResult(Result input)
